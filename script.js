2191675354ce3463da51504dc684f09d8ed418ce
George <georgea.fotheringham@gmail.com>
2022-09-11 22:00:11 -0400

    moves square linker to another function so it can be recalled

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function node (data, x, y, left = null, right = null, link) {
 
 
 }
-const board = [[1,1], [1,2], [1,3], [1,4], [1,5], [1,6], [1,7], [1,7], [1,8], [2,1], [2,2], [2,3], [2,4], [2,5], [2,6], [2,7], [2,8], [3,1], [3,2], [3,3], [3,4], [3,5], [3,6], [3,7], [3,8], [4,1], [4,2], [4,3], [4,4], [4,5], [4,6], [4,7], [4,8], [5,1], [5,2], [5,3], [5,4], [5,5], [5,6], [5,7], [5,8], [6,1], [6,2], [6,3], [6,4], [6,5], [6,6], [6,7], [6,8], [7,1], [7,2], [7,3], [7,4], [7,5], [7,6], [7,7],[7,8], [8,1], [8,2], [8,3], [8,4], [8,5], [8,6], [8,7], [8,8]];
+const board = [[1,1], [1,2], [1,3], [1,4], [1,5], [1,6], [1,7], [1,8], [2,1], [2,2], [2,3], [2,4], [2,5], [2,6], [2,7], [2,8], [3,1], [3,2], [3,3], [3,4], [3,5], [3,6], [3,7], [3,8], [4,1], [4,2], [4,3], [4,4], [4,5], [4,6], [4,7], [4,8], [5,1], [5,2], [5,3], [5,4], [5,5], [5,6], [5,7], [5,8], [6,1], [6,2], [6,3], [6,4], [6,5], [6,6], [6,7], [6,8], [7,1], [7,2], [7,3], [7,4], [7,5], [7,6], [7,7],[7,8], [8,1], [8,2], [8,3], [8,4], [8,5], [8,6], [8,7], [8,8]];
 allSquares = [];
 
 class tree { 
@@ -59,68 +59,74 @@ mkBoard (pointer = this.root, a = null, b = null) {
     if (pointer === null){
         return;
     }
-
-    const newSquare = new square(pointer.data[0], pointer.data[1], pointer)
     
+    const newSquare = new square(pointer.data[0], pointer.data[1], pointer)
+  
     {pointer.link = newSquare}
-
+    
   
    
     this.mkBoard(pointer.right);
     this.mkBoard(pointer.left);
 
-    a = newSquare.x + 1;
-    b = newSquare.y + 2;
+    newSquare.upOneRightTwo = this.linkElements(this.root, newSquare.x + 1, newSquare.y + 2)
+    newSquare.downOneLeftTwo = this.linkElements(this.root, newSquare.x - 1, newSquare.y - 2)
+    newSquare.upTwoRightOne = this.linkElements(this.root, newSquare.x + 2, newSquare.y + 1)
+    newSquare.downTwoLeftOne = this.linkElements(this.root, newSquare.x - 2, newSquare.y - 1)
+    newSquare.upOneLeftTwo = this.linkElements(this.root, newSquare.x + 1, newSquare.y - 2)
+    newSquare.downOneRightTwo = this.linkElements(this.root, newSquare.x - 1, newSquare.y + 2)
+    newSquare.upTwoLeftOne = this.linkElements(this.root, newSquare.x + 2, newSquare.y - 1)
+    newSquare.downTwoRightOne = this.linkElements(this.root, newSquare.x - 2, newSquare.y + 1)
+}
     
-    pointer = this.root;
+linkElements(pointer, a, b) {
    
-    console.log('goal', a, b)
-    console.log('prepointer', pointer)
-   try {while(pointer !== null  && pointer.x !== a, pointer.y !== b) {
+  
     
-       
-           if (a > 8 ||a < 1 || b > 8 || b < 1){
-            console.log('uh=oh')
-            pointer = null
-            break
-           }
-
-            
-            
-            if(a === pointer.x) {
-                if(b === pointer.y)
-                break
-                else if(b > pointer.y) {
-                pointer = pointer.right;
-                console.log('bigger b', pointer,b, pointer.y)
-                    continue}
-                pointer = pointer.left
-                console.log('smaller b', pointer, b, pointer.y)
-                continue
+    try {while(pointer !== null  && pointer.x !== a, pointer.y !== b) {
+     
+           
+            if (a > 8 ||a < 1 || b > 8 || b < 1){
+             pointer = null
+             break
             }
-        
-            else if (a > pointer.x) {
-            pointer = pointer.right;
-           console.log('bigger a', pointer)
-            continue
-            }
-    
-            pointer = pointer.left
-            console.log('smaller a', pointer)
-                continue
-            
-            
-        } } catch{console.log('broke')}
-        
-        newSquare.upOneRightTwo = pointer !== null ? pointer.link : null; 
-        console.log(newSquare)
-    
-       
-    
-}
-    
+ 
+             
+             if(a === pointer.x) {
+                 if(b === pointer.y)
+                 break
+                 else if(b > pointer.y) {
+                 pointer = pointer.right;
+                
+                     continue}
+                 pointer = pointer.left
+                 
+                 continue
+             }
+         
+             else if (a > pointer.x) {
+             pointer = pointer.right;
+           
+             continue
+             }
+     
+             pointer = pointer.left
+             
+                 continue
+             
+             
+         } } catch{console.log('broke')}
+                
+         return pointer !== null ? pointer.link : null
 }
 
+ movDistance (start, end, pointer = this.root.link) {
+
+    try{if (pointer.x === start[0] && pointer.y === start[1]){
+        console.log('runs searc ')
+ }catch{console.log('fails')}
+}
+}
 
   
 
@@ -139,6 +145,6 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
   }
  prettyPrint(myTree.root)
-
+  
   const myBoard = myTree.mkBoard();
-console.log(myBoard)
+  myTree.movDistance([5,1], [1, 1])
